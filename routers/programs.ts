801b8a605b59ae698aa827c6b7f53bbe9db5ae81
Bryant Cabrera <bcabrera@gocurrency.com>
2019-11-17 14:22:56 -0800

    Scaffolds programs router.

---
+++ routers/programs.ts
@@ -0,0 +1,20 @@
+/********** REQUIRES **********/
+const express = require('express');
+const router = express.Router();
+
+// const Program = require('../models/program');
+
+/********** MIDDLEWARE **********/
+
+/********** ROUTES **********/
+//Index Route
+
+//Create Route
+
+//Show Route
+
+//Update Route
+
+//Delete Route
+
+module.exports = router;
